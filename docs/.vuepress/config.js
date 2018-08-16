219e962570821de5917e5ad2f837443d40667fdb
Sidstumple <cydstumpel@msn.com>
2018-08-16 15:03:33 +0200

    Working version with custom colors

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+
+
+module.exports = {
+	title: "Henri's docs",
+	home: true,
+	description: "Henri's documentation",
+	themeConfig: {
+		logo: '/media/img/henris.svg',
+		repo: "matiseams/henris-docs",
+		editLinks: true,
+		docsDir: "docs",
+		lastUpdated: "Last Updated",
+		editLinkText: "Edit this page on GitHub",
+		nav: [{ text: "Matise", link: "https://www.matise.nl" }],
+		sidebar: [
+			["/getting-started/", "Getting started"],
+			["/typography/", "Typography"],
+			["/color/", "Color"],
+			["/grid/", "Grid"],
+			["/output/", "Output"]
+		]
+	},
+	configureWebpack: {
+		resolve: {
+			alias: {
+				"@media-img": "media/img/"
+			}
+		}
+	}
+};
---
+++ docs/getting-started/README.md
@@ -0,0 +1,10 @@
+# Getting started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+## Installing
+You can simply add Henris as a package to your project.
+
+```
+	$ npm i henris
+```
